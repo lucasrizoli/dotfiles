0b334b7f7eb6c4245b50f90fbf47aacf01716a18
lucasrizoli <lucas@lucasrizoli.com>
2021-05-20 19:34:30 -0400

    Keep git autoclrf to OS X; added rustup to upd8 alias

--- git/install.sh
+++ git/install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # Set up global Git preferences
 # From http://augustl.com/blog/2009/global_gitignores/
-git config --global core.excludesfile .gitignore
+
+	git config --global core.excludesfile .gitignore
+fi
 
 # Configure Git on OS X to properly handle line endings
 # https://help.github.com/articles/dealing-with-line-endings/
-git config --global core.autocrlf input
+if [[ $(uname -s) == "Darwin" ]]; then
+	git config --global core.autocrlf input
+fi
